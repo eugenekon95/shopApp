3131c1aae97df81db2a4d5666044ded4b887c954
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-12 16:44:39 +0200

    added showing products by categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,22 @@
 
 class CategoriesController < ApplicationController
+  # GET /categories
+  # GET /categories.json
+  def index
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @categories }
+    end
+  end
+
+  # GET /categories/1
+  # GET /categories/1.json
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products
+    
+   
+  end
 end
