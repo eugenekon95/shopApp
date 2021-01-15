835cb23596ec73d7618d2b1e89b0c5b51476861c
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-15 23:11:22 +0200

    ui fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,11 @@ module ApplicationHelper
     direction = params[:direction] == 'asc' ? 'desc' : 'asc'
     if search.nil?
       link_to title, { sort: column, direction: direction, max: min, min: max },
-              id: column.to_s, class: 'btn btn-outline-dark'
+              id: column.to_s, class: 'btn btn-outline-danger'
     else
 
       link_to title, search_categories_path( sort: column, direction: direction, max: min, min: max, product_name: search),
-              id: column.to_s, class: 'btn btn-outline-dark'
+              id: column.to_s, class: 'btn btn-outline-danger'
     end
   end
 end
