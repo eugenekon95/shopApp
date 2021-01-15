39d74dd7728f775b21484ace2eb40c82351537c3
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-15 17:35:14 +0200

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,25 +2,26 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery.easy-autocomplete
-//= require jquery-ui
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import 'jquery'
-import '@popperjs/core'
-import 'bootstrap'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
+
+
+
+
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 
 import '../stylesheets/application'
-
-
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-require("channels")
-
 
 
 
