0884c92de79f06de4cadab75f08c00fdd290e369
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-01-15 17:03:17 +0200

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,18 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//= require jquery.easy-autocomplete
+//= require jquery-ui
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
 import '@popperjs/core'
+import 'bootstrap'
 
+import '../stylesheets/application'
 
 
 
@@ -12,9 +21,7 @@ Turbolinks.start()
 ActiveStorage.start()
 require("channels")
 require("jquery")
-
 require("easy-autocomplete")
 import "./search"
 import "./jquery.easy-autocomplete"
-
 
