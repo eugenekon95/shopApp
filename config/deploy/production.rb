941498268882be94490d76aa285d4f90b719ebe6
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-02-04 22:09:29 +0200

    new DO droplet

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "159.65.112.215", user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+server "104.248.198.232", user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
 
 set :application, "store-x-shop"
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
@@ -6,5 +6,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :environment, "production"
 set :rails_env,   "production"
 
-set :nginx_server_name, "159.65.112.215"
+set :nginx_server_name, "104.248.198.232"
 set :puma_conf, "#{shared_path}/config/puma.rb"
