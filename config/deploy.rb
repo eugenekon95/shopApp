7d5d237c5ae137dc4598a5b8407b963c8ff92500
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2021-02-04 22:29:43 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,4 +21,4 @@ namespace :deploy do
   before 'check:linked_files', 'puma:jungle:setup'
 end
 after "deploy:finished", "nginx:restart"
-after "deploy:finished", "puma:start"
+after "deploy:finished", "puma:restart"
